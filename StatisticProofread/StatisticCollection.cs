bfa0f77745c928b2bdb52d07a003176b98001045
Aethelhelm <peter.lisulov@outlook.com>
2024-04-07 13:20:02 -0400

    Added new class StatisticCollection that inherits from List<T>, and where I can add new things

---
+++ StatisticProofread/StatisticCollection.cs
@@ -0,0 +1,13 @@
+﻿using PFDB.StatisticUtility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PFDB.Proofreading
+{
+	public class StatisticCollection : List<IStatistic>
+	{
+	}
+}

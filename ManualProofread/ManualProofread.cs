8e8418acce5e5d64b1e81603ae5df5ce3d40a3d3
Aethelhelm <peter.lisulov@outlook.com>
2024-12-20 13:35:41 -0500

    added manual proofreader thing, also updating nuget packages for security

---
+++ ManualProofread/ManualProofread.cs
@@ -0,0 +1,46 @@
+﻿using PFDB;
+using PFDB.Proofreading;
+using PFDB.Parsing;
+using PFDB.WeaponStructure;
+using PFDB.WeaponUtility;
+using PFDB.ConversionUtility;
+using PFDB.StatisticUtility;
+using PFDB.Logging;
+using PFDB.ManualProofread;
+using PFDB.SQLite;
+
+ManualProofread proofread = new ManualProofread(PhantomForcesDataModel.GetWeaponCollection(new PhantomForcesVersion("10.1.1")));
+
+
+namespace PFDB{
+    namespace ManualProofread{
+		class ManualProofread{
+			public ManualProofread(IWeaponCollection weaponcollection){
+				foreach (IWeapon weapon in weaponcollection.Weapons)
+				{
+					if (weapon.NeedsRevision == false) continue;
+					foreach(IConversion conversion in weapon.ConversionCollection.Conversions)
+					{
+						if(conversion.NeedsRevision == false) continue;
+						foreach(IStatistic statistic in conversion.StatisticCollection.Statistics)
+						{
+							if(statistic.NeedsRevision == false) continue;
+							foreach(string str in statistic.Statistics)
+							{
+								Console.WriteLine(str);
+								string newstr = Console.ReadLine() ?? str;
+								Console.WriteLine($"The new statistic has {(str == newstr ? "not" : "")} been altered{(str == newstr ? "." : "to " + newstr)}");
+								PFDBLogger.LogDebug($"The new statistic has {(str == newstr ? "not" : "")} been altered{(str == newstr ? "." : "to " + newstr)}", parameter: [ str, newstr]);
+							}
+						}
+					}
+				}
+
+			}
+			
+
+
+		}
+	
+    }
+}
